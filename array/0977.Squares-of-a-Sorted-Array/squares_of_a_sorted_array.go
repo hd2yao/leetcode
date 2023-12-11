2aeaa04d9d11cc930b6002bdca906612d4058a91
hd2yao <1054290355@qq.com>
2023-12-11 11:29:42 +0800

    feat(squares_of_a_sorted_array):simplifing 0977 code

--- array/0977.Squares-of-a-Sorted-Array/squares_of_a_sorted_array.go
+++ array/0977.Squares-of-a-Sorted-Array/squares_of_a_sorted_array.go
@@ -32,3 +32,21 @@ func abs(num int) int {
 func square(num int) int {
 	return num * num
 }
+
+// 简化上面的代码
+func sortedSquaresSimple(nums []int) []int {
+	res := make([]int, len(nums))
+	index := len(nums) - 1
+	left, right := 0, len(nums)-1
+	for left <= right {
+		if nums[left]*nums[left] < nums[right]*nums[right] {
+			res[index] = nums[right] * nums[right]
+			right--
+		} else {
+			res[index] = nums[left] * nums[left]
+			left++
+		}
+		index--
+	}
+	return res
+}
