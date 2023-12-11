467b1fbde14aec22913f2ba9244bad55a4dd3fa1
hd2yao <1054290355@qq.com>
2023-12-11 11:09:00 +0800

    feat(squares_of_a_sorted_array):error in [-5,-3,-2,-1]

--- array/0977.Squares-of-a-Sorted-Array/squares_of_a_sorted_array.go
+++ array/0977.Squares-of-a-Sorted-Array/squares_of_a_sorted_array.go
@@ -1,1 +1,38 @@
 package _977_Squares_of_a_Sorted_Array
+
+func sortedSquares(nums []int) []int {
+    res := make([]int, len(nums))
+    left, right := 0, len(nums)
+    for left < right {
+        right--
+        if left == right {
+            res[left] = square(nums[left])
+            break
+        }
+        if abs(nums[left]) < abs(nums[right]) {
+            res[right] = square(nums[right])
+            continue
+        }
+        res[left], res[right] = nums[right], square(nums[left])
+
+    }
+    return res
+}
+
+func abs(num int) int {
+    if num < 0 {
+        return -num
+    }
+    return num
+}
+
+func square(num int) int {
+    return num * num
+}
+
+func returnResult(a, b int) int {
+    if abs(a) < abs(b) {
+        return square(a)
+    }
+    return square(b)
+}
