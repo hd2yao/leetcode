c26cc189500fce9a247081e8c0f959156e9b8fbe
hd2yao <1054290355@qq.com>
2023-12-12 11:08:07 +0800

    feat(minimum_size_subarray_sum):finish minimum_size_subarray_sum by 滑动窗口

--- array/0209.Minimum-Size-Subarray-Sum/minimum_size_subarray_sum.go
+++ array/0209.Minimum-Size-Subarray-Sum/minimum_size_subarray_sum.go
@@ -1,1 +1,27 @@
 package _209_Minimum_Size_Subarray_Sum
+
+// 滑动窗口
+func minSubArrayLen(target int, nums []int) int {
+    left, right := 0, 0
+    sum := nums[0]
+    subArrayLens := 0
+    for left <= right && right < len(nums) {
+        //fmt.Println(left, right, sum, subArrayLens)
+        if sum >= target {
+            if subArrayLens == 0 || (subArrayLens != 0 && subArrayLens > right-left+1) {
+                subArrayLens = right - left + 1
+            }
+            sum -= nums[left]
+            left++
+        } else {
+            if right == len(nums)-1 {
+                break
+            }
+            right++
+            sum += nums[right]
+        }
+
+    }
+    //fmt.Println("final", left, right, sum, subArrayLens)
+    return subArrayLens
+}
