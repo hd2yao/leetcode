df88b30a3fd5c0eea053b8ee4fe4028d04160b60
hd2yao <1054290355@qq.com>
2023-12-19 17:45:07 +0800

    feat(design-linked-list):write code by myself

---
+++ linked-list/0707.Design-Linked-List/design_linked_list.go
@@ -0,0 +1,78 @@
+package _707_Design_Linked_List
+
+type MyLinkedList struct {
+    Len       int
+    DummyHead *Node
+}
+
+type Node struct {
+    Val  int
+    Next *Node
+}
+
+func Constructor() MyLinkedList {
+    return MyLinkedList{
+        Len:       0,
+        DummyHead: &Node{},
+    }
+}
+
+func (this *MyLinkedList) Get(index int) int {
+    if index >= this.Len {
+        return -1
+    }
+    count := 0
+    cur := this.DummyHead
+    for count < index {
+        cur = cur.Next
+        count++
+    }
+    return cur.Next.Val
+}
+
+func (this *MyLinkedList) AddAtHead(val int) {
+    if this.DummyHead.Next == nil {
+        this.DummyHead.Next = &Node{Val: val}
+    } else {
+        //cur := this.DummyHead.Next
+        this.DummyHead.Next = &Node{Val: val, Next: this.DummyHead.Next.Next}
+    }
+    this.Len++
+}
+
+func (this *MyLinkedList) AddAtTail(val int) {
+    cur := this.DummyHead
+    for cur.Next != nil {
+        cur = cur.Next
+    }
+    cur.Next = &Node{Val: val, Next: nil}
+    this.Len++
+}
+
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+    if this.Get(index) != -1 {
+        cur := this.DummyHead
+        count := 0
+        for count < index {
+            cur = cur.Next
+            count++
+        }
+        cur.Next = &Node{Val: val, Next: cur.Next.Next}
+        this.Len++
+    } else if this.Len == index {
+        this.AddAtTail(val)
+    }
+}
+
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+    if this.Get(index) != -1 {
+        cur := this.DummyHead
+        count := 0
+        for count < index {
+            cur = cur.Next
+            count++
+        }
+        cur.Next = cur.Next.Next
+        this.Len--
+    }
+}
