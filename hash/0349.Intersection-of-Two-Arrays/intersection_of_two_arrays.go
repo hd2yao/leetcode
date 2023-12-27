67099b58a71779fae19d46b680cdbdc37450d4e8
hd2yao <1054290355@qq.com>
2023-12-27 13:11:44 +0800

    feat(intersection):optimize code

--- hash/0349.Intersection-of-Two-Arrays/intersection_of_two_arrays.go
+++ hash/0349.Intersection-of-Two-Arrays/intersection_of_two_arrays.go
@@ -1,22 +1,22 @@
 package _349_Intersection_of_Two_Arrays
 
 func intersection(nums1 []int, nums2 []int) []int {
-	numMap := make(map[int]int, len(nums1)+len(nums2))
-	for _, value := range nums1 {
-		if _, ok := numMap[value]; !ok {
-			numMap[value]++
-		}
-	}
-	for _, value := range nums2 {
-		if _, ok := numMap[value]; ok {
-			numMap[value]--
-		}
-	}
-	result := []int{}
-	for value, count := range numMap {
-		if count <= 0 {
-			result = append(result, value)
-		}
-	}
-	return result
+    numMap := make(map[int]int, len(nums1)+len(nums2))
+    for _, value := range nums1 {
+        if _, ok := numMap[value]; !ok {
+            numMap[value] = 1
+        }
+    }
+    for _, value := range nums2 {
+        if _, ok := numMap[value]; ok {
+            numMap[value] = 0
+        }
+    }
+    result := []int{}
+    for value, count := range numMap {
+        if count == 0 {
+            result = append(result, value)
+        }
+    }
+    return result
 }
