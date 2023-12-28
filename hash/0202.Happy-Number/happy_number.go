534ae6bb8623a0b981c5f3be13258bfe1650c185
hd2yao <1054290355@qq.com>
2023-12-28 13:48:23 +0800

    optimize(happy_number):optimize 0202 code

--- hash/0202.Happy-Number/happy_number.go
+++ hash/0202.Happy-Number/happy_number.go
@@ -23,3 +23,22 @@ func sumFunc(num, sum int) int {
     res := num / 10
     return sumFunc(res, sum)
 }
+
+// 代码精简
+
+func isHappyOptimize(n int) bool {
+    numMap := make(map[int]bool)
+    for n != 1 && !numMap[n] {
+        n, numMap[n] = getSum(n), true
+    }
+    return n == 1
+}
+
+func getSum(n int) int {
+    sum := 0
+    for n > 0 {
+        sum += (n % 10) * (n % 10)
+        n = n / 10
+    }
+    return sum
+}
