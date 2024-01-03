c8e6c384a7275c14db9494bd6995bc624507cae4
hd2yao <1054290355@qq.com>
2024-01-03 09:54:17 +0800

    feat(ransom_note):add 0383 code

--- hash/0383.Ransom-Note/ransom_note.go
+++ hash/0383.Ransom-Note/ransom_note.go
@@ -1,1 +1,16 @@
 package _383_Ransom_Note
+
+func canConstruct(ransomNote string, magazine string) bool {
+	magazineMap := make(map[byte]int, len(magazine))
+	for _, a := range magazine {
+		magazineMap[byte(a)]++
+	}
+	for _, b := range ransomNote {
+		counts, ok := magazineMap[byte(b)]
+		if counts == 0 || !ok {
+			return false
+		}
+		magazineMap[byte(b)]--
+	}
+	return true
+}
