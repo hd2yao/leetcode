08601a8614f9f3b0c477608c1315ff73b9aaad01
hd2yao <1054290355@qq.com>
2024-01-05 15:42:21 +0800

    feat(reverse_string):can delete if

--- string/0344.Reverse-String/reverse_string.go
+++ string/0344.Reverse-String/reverse_string.go
@@ -1,12 +1,10 @@
 package _344_Reverse_String
 
 func reverseString(s []byte) {
-    if len(s) > 1 {
-        left, right := 0, len(s)-1
-        for left < right {
-            s[left], s[right] = s[right], s[left]
-            left++
-            right--
-        }
+    left, right := 0, len(s)-1
+    for left < right {
+        s[left], s[right] = s[right], s[left]
+        left++
+        right--
     }
 }
