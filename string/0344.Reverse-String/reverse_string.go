27261ff614dea27bdddbbc80e9173d23dd282bfb
hd2yao <1054290355@qq.com>
2024-01-05 15:37:34 +0800

    feat(reverse_string):two pointer solving

--- string/0344.Reverse-String/reverse_string.go
+++ string/0344.Reverse-String/reverse_string.go
@@ -1,5 +1,12 @@
 package _344_Reverse_String
 
 func reverseString(s []byte) {
-
+    if len(s) > 1 {
+        left, right := 0, len(s)-1
+        for left < right {
+            s[left], s[right] = s[right], s[left]
+            left++
+            right--
+        }
+    }
 }
