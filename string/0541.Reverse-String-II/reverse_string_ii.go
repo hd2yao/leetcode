c22d5a70f3443465434b47b195e5a9cb83731d1b
hd2yao <1054290355@qq.com>
2024-01-08 16:25:47 +0800

    feat(0541):finish reverse string ii by loop

--- string/0541.Reverse-String-II/reverse_string_ii.go
+++ string/0541.Reverse-String-II/reverse_string_ii.go
@@ -1,1 +1,36 @@
 package _541_Reverse_String_II
+
+func reverseStr(s string, k int) string {
+    var result string
+    var resultSlice []string
+
+    for i := 0; i < len(s); i += 2 * k {
+        end := i + 2*k
+        if end > len(s) {
+            end = len(s)
+        }
+        resultSlice = append(resultSlice, s[i:end])
+    }
+    for i := 0; i < len(resultSlice)-1; i++ {
+        result += reverseString([]byte(resultSlice[i][:k])) + resultSlice[i][k:2*k]
+    }
+
+    if len(resultSlice[len(resultSlice)-1]) < k {
+        reverseString([]byte(resultSlice[len(resultSlice)-1]))
+        result += reverseString([]byte(resultSlice[len(resultSlice)-1]))
+    } else {
+        result += reverseString([]byte(resultSlice[len(resultSlice)-1][:k])) + resultSlice[len(resultSlice)-1][k:]
+    }
+
+    return result
+}
+
+func reverseString(s []byte) string {
+    left, right := 0, len(s)-1
+    for left < right {
+        s[left], s[right] = s[right], s[left]
+        left++
+        right--
+    }
+    return string(s)
+}
