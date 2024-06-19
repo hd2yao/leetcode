35d380b8a499d82b4f17505bc5ff92868a6a8a2c
hd2yao <1054290355@qq.com>
2024-06-19 13:39:03 +0800

    optimize: 优化双指针代码

--- training/day2/0209_minimum_size_subarray_sum.go
+++ training/day2/0209_minimum_size_subarray_sum.go
@@ -1,23 +1,20 @@
 package day2
 
+// 滑动窗口-双指针
 func minSubArrayLen(target int, nums []int) int {
     // 分别为子数组起始下标、子数组终止下标、子数组总和、目标子数组的最小长度
-    left, right, sum, length := 0, 0, 0, 0
+    left, right, length := 0, 0, 0
+    sum := nums[0]
     for left < len(nums) || right < len(nums) {
-        if left == 0 && right == 0 {
-            sum = nums[0]
-        }
         if sum < target {
             right++
             if right == len(nums) {
                 break
             }
             sum += nums[right]
-        } else if sum >= target {
+        } else {
             curLen := right - left + 1
-            if length == 0 {
-                length = curLen
-            } else if curLen < length {
+            if length == 0 || (length != 0 && curLen < length) {
                 length = curLen
             }
             sum -= nums[left]
