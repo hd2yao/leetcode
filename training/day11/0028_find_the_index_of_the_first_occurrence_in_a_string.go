580770955df472917718ccd3ceffd156385a5dcc
hd2yao <1054290355@qq.com>
2024-07-01 10:45:14 +0800

    暂时跳过

--- training/day11/0028_find_the_index_of_the_first_occurrence_in_a_string.go
+++ training/day11/0028_find_the_index_of_the_first_occurrence_in_a_string.go
@@ -1,30 +1,30 @@
-package main
+package day11
 
 func strStr(haystack string, needle string) int {
-    return 0
+	return 0
 }
 
 func getNext(nextArray []int, needle string) {
-    nextArray[0] = 0
-    for i := 0; i < len(needle); i++ {
-        if i == 0 {
-            continue
-        }
-        subStrMap := make(map[string]int, 0)
-        for j := 0; j < i; j++ {
-            subStrMap[needle[:j+1]] = 1
-        }
-        for j := i; j > 0; j-- {
-            if _, ok := subStrMap[needle[j:i+1]]; ok {
-                nextArray[i] = max(nextArray[i], len(needle[1:j]))
-            }
-        }
-    }
+	nextArray[0] = 0
+	for i := 0; i < len(needle); i++ {
+		if i == 0 {
+			continue
+		}
+		subStrMap := make(map[string]int, 0)
+		for j := 0; j < i; j++ {
+			subStrMap[needle[:j+1]] = 1
+		}
+		for j := i; j > 0; j-- {
+			if _, ok := subStrMap[needle[j:i+1]]; ok {
+				nextArray[i] = max(nextArray[i], len(needle[1:j]))
+			}
+		}
+	}
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
