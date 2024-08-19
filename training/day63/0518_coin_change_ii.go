7cc46ce2e71467992c09acff7eb31cd30aaf172c
hd2yao <1054290355@qq.com>
2024-08-19 14:14:59 +0800

    feat: 降维成一维 dp 解决

--- training/day63/0518_coin_change_ii.go
+++ training/day63/0518_coin_change_ii.go
@@ -18,3 +18,16 @@ func change(amount int, coins []int) int {
     }
     return dp[len(coins)][amount]
 }
+
+// 完全背包 一维 dp
+func changeOneDimension(amount int, coins []int) int {
+    dp := make([]int, amount+1)
+    dp[0] = 1
+    for i := 0; i < len(coins); i++ {
+        for j := coins[i]; j <= amount; j++ {
+            dp[j] += dp[j-coins[i]]
+        }
+    }
+
+    return dp[amount]
+}
