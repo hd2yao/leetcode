932d2456b82ace723f80f2aa824b8ddadc1553fb
hd2yao <1054290355@qq.com>
2024-09-11 16:04:37 +0800

    feat(0027_test): 测试用例中调整打印方式

--- second-training/day1/0027.Remove-Element/0027_remove_element_test.go
+++ second-training/day1/0027.Remove-Element/0027_remove_element_test.go
@@ -1,1 +1,66 @@
 package _027_Remove_Element
+
+import (
+    "fmt"
+    "testing"
+)
+
+type question0027 struct {
+    paras
+    ans
+}
+
+type paras struct {
+    nums   []int
+    target int
+}
+
+type ans struct {
+    result int
+}
+
+func TestRemoveElement(t *testing.T) {
+    testCases := []question0027{
+        {
+            paras{[]int{1, 0, 1}, 1},
+            ans{1},
+        },
+        {
+            paras{[]int{0, 1, 0, 3, 0, 12}, 0},
+            ans{3},
+        },
+
+        {
+            paras{[]int{0, 1, 0, 3, 0, 0, 0, 0, 1, 12}, 0},
+            ans{4},
+        },
+
+        {
+            paras{[]int{0, 0, 0, 0, 0}, 0},
+            ans{0},
+        },
+
+        {
+            paras{[]int{1}, 1},
+            ans{0},
+        },
+
+        {
+            paras{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2},
+            ans{5},
+        },
+    }
+
+    fmt.Printf("------------------------Leetcode Problem 27------------------------\n")
+
+    for _, testCase := range testCases {
+        //fmt.Printf("【input】:%v       【output】:%v\n", testCase.paras, removeElement(testCase.paras.nums, testCase.paras.target))
+        // 打印原始的 input
+        fmt.Printf("【input】:%v ", testCase.paras)
+
+        // 调用 removeElement 并打印结果
+        result := removeElement(testCase.paras.nums, testCase.paras.target)
+        fmt.Printf("【output】:%v\n", result)
+    }
+    fmt.Printf("\n\n\n")
+}
